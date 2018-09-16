ddaf591ac3cfd6cc333473728929f727764c736f
rgx6 <rgx@hotmail.co.jp>
2018-09-17 02:15:28 +0900

    なんとなくVue.js化。

--- index.js
+++ index.js
@@ -1,79 +1,81 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-
-    var BASE_COLORS = [
-        '#cebf94',
-        '#cccccc',
-    ];
-
-    var STYLE_FILE_MAPS = [
-        [
-            'image/c0/p11.png',
-            'image/c0/p21.png',
-            'image/c0/p31.png',
-            'image/c0/p41.png',
-            'image/c0/p51.png',
-            // 'image/c0/p61.png',
-
-            'image/c0/p12.png',
-            'image/c0/p22.png',
-            'image/c0/p32.png',
-            'image/c0/p42.png',
-            'image/c0/p52.png',
-            // 'image/c0/p62.png',
-
-            'image/c0/p13.png',
-            'image/c0/p23.png',
-            'image/c0/p33.png',
-            'image/c0/p43.png',
-            'image/c0/p53.png',
-            // 'image/c0/p63.png',
-
-            'image/c0/p14.png',
-            'image/c0/p24.png',
-            'image/c0/p34.png',
-            'image/c0/p44.png',
-            'image/c0/p54.png',
-            // 'image/c0/p64.png',
+var vm = new Vue({
+    el: '#content',
+    data: {
+        BASE_COLORS: [
+            '#cebf94',
+            '#cccccc',
         ],
-        [
-            'image/c1/p11.png',
-            'image/c1/p21.png',
-            'image/c1/p31.png',
-            'image/c1/p41.png',
-            'image/c1/p51.png',
-            // 'image/c1/p61.png',
+        STYLE_FILE_MAPS: [
+            [
+                'image/c0/p11.png',
+                'image/c0/p21.png',
+                'image/c0/p31.png',
+                'image/c0/p41.png',
+                'image/c0/p51.png',
+                // 'image/c0/p61.png',
 
-            'image/c1/p12.png',
-            'image/c1/p22.png',
-            'image/c1/p32.png',
-            'image/c1/p42.png',
-            'image/c1/p52.png',
-            // 'image/c1/p62.png',
+                'image/c0/p12.png',
+                'image/c0/p22.png',
+                'image/c0/p32.png',
+                'image/c0/p42.png',
+                'image/c0/p52.png',
+                // 'image/c0/p62.png',
 
-            'image/c1/p13.png',
-            'image/c1/p23.png',
-            'image/c1/p33.png',
-            'image/c1/p43.png',
-            'image/c1/p53.png',
-            // 'image/c1/p63.png',
+                'image/c0/p13.png',
+                'image/c0/p23.png',
+                'image/c0/p33.png',
+                'image/c0/p43.png',
+                'image/c0/p53.png',
+                // 'image/c0/p63.png',
 
-            'image/c1/p14.png',
-            'image/c1/p24.png',
-            'image/c1/p34.png',
-            'image/c1/p44.png',
-            'image/c1/p54.png',
-            // 'image/c1/p64.png',
+                'image/c0/p14.png',
+                'image/c0/p24.png',
+                'image/c0/p34.png',
+                'image/c0/p44.png',
+                'image/c0/p54.png',
+                // 'image/c0/p64.png',
+            ],
+            [
+                'image/c1/p11.png',
+                'image/c1/p21.png',
+                'image/c1/p31.png',
+                'image/c1/p41.png',
+                'image/c1/p51.png',
+                // 'image/c1/p61.png',
+
+                'image/c1/p12.png',
+                'image/c1/p22.png',
+                'image/c1/p32.png',
+                'image/c1/p42.png',
+                'image/c1/p52.png',
+                // 'image/c1/p62.png',
+
+                'image/c1/p13.png',
+                'image/c1/p23.png',
+                'image/c1/p33.png',
+                'image/c1/p43.png',
+                'image/c1/p53.png',
+                // 'image/c1/p63.png',
+
+                'image/c1/p14.png',
+                'image/c1/p24.png',
+                'image/c1/p34.png',
+                'image/c1/p44.png',
+                'image/c1/p54.png',
+                // 'image/c1/p64.png',
+            ],
         ],
-    ];
-
-    var styleImages = [];
-
-    init();
-
-    function init() {
-        STYLE_FILE_MAPS.forEach((styleFileMap) => {
+        canvasHeight: 1080,
+        canvasWidth: 1920,
+        staNumber: 5000,
+        staScale: 1.0,
+        colorIndex: 0,
+        canvas: null,
+        context: null,
+        styleImages: [],
+    },
+    created: function () {
+        this.STYLE_FILE_MAPS.forEach((styleFileMap) => {
             var images = [];
 
             styleFileMap.forEach((file) => {
@@ -82,50 +84,35 @@ document.addEventListener("DOMContentLoaded", function () {
                 images.push(img);
             });
 
-            styleImages.push(images);
+            this.styleImages.push(images);
         });
-    }
+    },
+    methods: {
+        generate: function () {
+            this.canvas = document.getElementById('canvas');
+            this.context = this.canvas.getContext('2d');
 
-    function generate() {
-        var num = document.getElementById('number').value;
-        var scale = document.getElementById('scale').value;
-        var colorIndex = getRadioValue('color');
+            var images = this.styleImages[this.colorIndex];
 
-        var images = styleImages[colorIndex];
+            this.context.fillStyle = this.BASE_COLORS[this.colorIndex];
+            this.context.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
 
-        context.fillStyle = BASE_COLORS[colorIndex];
-        context.fillRect(0, 0, canvas.width, canvas.height);
+            for (var i = 0; i < this.staNumber; i++) {
+                var image = images[this.getRandom(images.length)];
 
-        for (var i = 0; i < num; i++) {
-            var image = images[getRandom(images.length)];
+                var x = this.getRandom(this.canvasWidth + image.width) - image.width;
+                var y = this.getRandom(this.canvasHeight + image.height) - image.height;
 
-            var x = getRandom(canvas.width + image.width) - image.width;
-            var y = getRandom(canvas.height + image.height) - image.height;
+                var inverse = Math.round(Math.random()) === 1 ? -1 : 1;
 
-            var inverse = Math.round(Math.random()) === 1 ? -1 : 1;
-
-            context.save();
-            context.scale(scale * inverse, scale);
-            context.drawImage(image, (x * inverse) / scale, y / scale);
-            context.restore();
-        }
-    }
-
-    function getRadioValue(name) {
-        var elements = document.getElementsByName(name);
-
-        for (var i = 0; i < elements.length; i++) {
-            if (elements[i].checked) return elements[i].value;
-        }
-
-        return 0;
-    }
-
-    function getRandom(value) {
-        return Math.floor(Math.random() * value);
-    }
-
-    document.getElementById('generate').addEventListener('click', function () {
-        generate();
-    });
+                this.context.save();
+                this.context.scale(this.staScale * inverse, this.staScale);
+                this.context.drawImage(image, (x * inverse) / this.staScale, y / this.staScale);
+                this.context.restore();
+            }
+        },
+        getRandom: function (value) {
+            return Math.floor(Math.random() * value);
+        },
+    },
 });
